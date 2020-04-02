169faa237768669ade52cf82527b636d5c13779c
GwenRspl <gwenaelle.rispal@gmail.com>
2020-04-02 11:17:08 +0200

    add enemy chase and attack AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,9 +7,10 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour {
 
     [SerializeField] Transform target;
-    [SerializeField] float chaseRange = 5f;
+    [SerializeField] float chaseRange = 10f;
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     void Start () {
         navMeshAgent = GetComponent<NavMeshAgent> ();
@@ -16,8 +18,34 @@ public class EnemyAI : MonoBehaviour {
 
     void Update () {
         distanceToTarget = Vector3.Distance (target.position, transform.position);
-        if (distanceToTarget <= chaseRange) {
-            navMeshAgent.SetDestination (target.transform.position);
+
+        if (isProvoked) {
+            EngageTarget ();
+        } else if (distanceToTarget <= chaseRange) {
+            isProvoked = true;
         }
+    }
+
+    private void EngageTarget () {
+        if (distanceToTarget > navMeshAgent.stoppingDistance) {
+            ChaseTarget ();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+            AttackTarget ();
+        }
+    }
+
+    private void ChaseTarget () {
+        navMeshAgent.SetDestination (target.position);
+    }
+
+    private void AttackTarget () {
+        print ("GRAAAHHH");
+    }
+
+    void OnDrawGizmosSelected () {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere (transform.position, chaseRange);
     }
 }
