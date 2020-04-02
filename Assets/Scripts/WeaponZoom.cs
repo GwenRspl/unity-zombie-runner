2f979a2b0775d0dc54dbea02660d2d59dce6fcca
GwenRspl <gwenaelle.rispal@gmail.com>
2020-04-02 22:37:01 +0200

    add zoom on right clic

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour {
+
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+
+    bool zoomedInToggle = false;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            if (!zoomedInToggle) {
+                zoomedInToggle = true;
+                this.fpsCamera.fieldOfView = this.zoomedInFOV;
+            } else {
+                zoomedInToggle = false;
+                this.fpsCamera.fieldOfView = this.zoomedOutFOV;
+            }
+        }
+    }
+
+}
