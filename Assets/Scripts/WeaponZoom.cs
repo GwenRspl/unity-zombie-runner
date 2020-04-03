f0150b959d05f4ad0d4df7d590fa8241b00ca61e
GwenRspl <gwenaelle.rispal@gmail.com>
2020-04-03 11:08:23 +0200

    change mouse sensitivity on zoom

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,23 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour {
 
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = .5f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomedInToggle = false;
+
+    private void Start() {
+        this.fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update() {
         if (Input.GetMouseButtonDown(1)) {
             if (!zoomedInToggle) {
                 zoomedInToggle = true;
                 this.fpsCamera.fieldOfView = this.zoomedInFOV;
+                this.fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                this.fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
             } else {
                 zoomedInToggle = false;
                 this.fpsCamera.fieldOfView = this.zoomedOutFOV;
+                this.fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                this.fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
             }
         }
     }
